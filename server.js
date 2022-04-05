dc11c67fac0880b7df0e884df61ecb5128f846a0
coyzRepo <coyz.ratunil@gmail.com>
2022-04-05 17:38:47 +0800

    mix sendEmail on the server.js file

--- server.js
+++ server.js
@@ -7,8 +7,9 @@ const morgan = require('morgan');
 const app = express();
 const bodyParser = require('body-parser');
 const services = require('./services');
-const axios = require('axios');
-
+require('dotenv').config();
+const nodemailer = require('nodemailer');
+let schedule = require('node-schedule');
 
 
 app.use(bodyParser());
@@ -28,16 +29,58 @@ app.get('/', (request, response) => {
 app.post('/', async (request, response) => {
 
   try {
-        // console.log('I AM HERE', request.body);
+
+    let user = process.env.EMAILUSERNAME;
+    let pass = process.env.EMAILPASS;
+
+    console.log('I AM HERE', request.body);
+
 
     const { userid: userId, wish } = request.body;
     const { validateUser } = services;
     
     if (!userId) {
+      console.log('Here!');
       return response.status(400).sendFile(__dirname + '/views/error.html');
     }
 
     const isValidUser = await validateUser(userId);
+
+    const sendMail = () => {
+      // Create a SMTP transporter object
+      let transporter = nodemailer.createTransport({
+        host: 'smtp.ethereal.email',
+        port: 587,
+        auth: {
+            user: `${user}`,
+            pass: `${pass}`
+        }
+      });
+
+        // Message object 
+        let message = {
+          from: 'do_not_reply@northpole.com',
+          to: 'santa@northpole.com',
+          subject: 'Glitch Email',
+          username: userId,
+          text: wish
+        };
+
+      schedule.scheduleJob('*/15 * * * * *', () => {
+            transporter.sendMail(message, (err, info) => {
+                if (err) {
+                    console.log('Error occurred. ' + err.message);
+                    return process.exit(1);
+                }
+        
+                console.log('Message sent: %s', info.messageId);
+                // Preview only available when sending through an Ethereal account
+                console.log('Preview URL: %s', nodemailer.getTestMessageUrl(info));
+            });
+        })
+    }
+
+    sendMail();
     
     if (isValidUser) {
       // show success page
@@ -56,4 +99,5 @@ app.post('/', async (request, response) => {
 // listen for requests :)
 const listener = app.listen(process.env.PORT || 3000, () => {
   console.log('Your app is listening on port ' + listener.address().port);
+
 });
